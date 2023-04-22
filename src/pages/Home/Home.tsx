90a46e27b2266cda41be5ee9ef29c8d0c7cdb5ed
Boydem <noyemdahan@gmail.com>
2023-04-22 19:12:24 +0300

    better responsive home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -26,28 +26,30 @@ export function Home() {
             initial={'hidden'}
             animate={'visible'}
             exit={'hidden'}
-            className='home'
+            className='home flex flex-center'
         >
-            <motion.header
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.55, ease: 'easeInOut' }}
-                className='header layout-padding-inline-xl flex justify-between align-end'
-            >
-                <Logo size={'large'} />
-                <LightDarkButtons />
-            </motion.header>
-            <Gallery items={projects} />
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.5 }}
-                className='outro layout-padding-inline-xl'
-            >
-                <Text type='title' size='small'>
-                    Frontend developer <br /> based in Tel Aviv.
-                </Text>
-            </motion.div>
+            <div className='container'>
+                <motion.header
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.55, ease: 'easeInOut' }}
+                    className='header layout-padding-inline-xl flex justify-between align-end'
+                >
+                    <Logo size={'large'} />
+                    <LightDarkButtons />
+                </motion.header>
+                <Gallery items={projects} />
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.5 }}
+                    className='outro layout-padding-inline-xl'
+                >
+                    <Text type='title' size='small'>
+                        Frontend developer <br /> based in Tel Aviv.
+                    </Text>
+                </motion.div>
+            </div>
         </motion.section>
     )
 }
--- src/pages/Home/parts/Gallery/Gallery.tsx
+++ src/pages/Home/parts/Gallery/Gallery.tsx
@@ -16,9 +16,6 @@ export function Gallery({ items }: Props) {
     const { isTouchDevice } = useSelector((storeState: RootState) => storeState.systemModule)
     const { mousePos } = useSelector((storeState: RootState) => storeState.systemModule)
 
-    // const [touchStartX, setTouchStartX] = useState<number | null>(null)
-    // const [touchCurrentX, setTouchCurrentX] = useState<number | null>(null)
-
     useEffect(() => {
         function handleGalleryMove(clientX: number) {
             if (isTouchDevice) return
@@ -44,42 +41,6 @@ export function Gallery({ items }: Props) {
         }
     }, [mousePos.x, isTouchDevice])
 
-    // function handleTouchStart(event: React.TouchEvent) {
-    //     setTouchStartX(event.touches[0].clientX)
-    //     setTouchCurrentX(event.touches[0].clientX)
-    // }
-
-    // function handleTouchMove(event: React.TouchEvent) {
-    //     if (touchStartX === null || touchCurrentX === null) return
-    //     console.log('touchCurrentX:', touchCurrentX)
-    //     console.log('event:', event)
-    //     const clientX = event.touches[0].clientX
-    //     const deltaX = clientX - touchCurrentX
-    //     setTouchCurrentX(clientX)
-
-    //     const shouldHandleGalleryMove = contentRef.current && innerRef.current
-    //     if (!shouldHandleGalleryMove) return
-    //     if (!innerRef.current) return
-
-    //     const innerWidth = innerRef.current.offsetWidth
-    //     const contentWidth = contentRef.current.offsetWidth
-    //     const start = innerRef.current.offsetLeft
-    //     const end = innerWidth + innerRef.current.offsetLeft
-    //     console.log('start,end:', start, end)
-    //     const totalDistance = contentWidth - innerWidth
-    //     if (clientX < start) setTranslateX(0)
-    //     else if (clientX > end) setTranslateX(-totalDistance)
-    //     else if (clientX > start && clientX < end) {
-    //         const percent = (clientX - start) / (end - start)
-    //         const newTranslateX = -percent * totalDistance + deltaX
-    //         setTranslateX(Math.max(Math.min(newTranslateX, 0), -totalDistance))
-    //     }
-    // }
-
-    // function handleTouchEnd() {
-    //     setTouchStartX(null)
-    //     setTouchCurrentX(null)
-    // }
     return (
         <motion.section
             initial={{ opacity: 0 }}
@@ -87,13 +48,10 @@ export function Gallery({ items }: Props) {
             transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.5 }}
             className='gallery layout-padding-inline disable-scrollbar'
         >
-            <div ref={innerRef} className='inner'>
+            <div ref={innerRef} className='inner disable-scrollbar'>
                 <div
-                    // onTouchStart={handleTouchStart}
-                    // onTouchMove={handleTouchMove}
-                    // onTouchEnd={handleTouchEnd}
                     ref={contentRef}
-                    className='contnet'
+                    className='contnet disable-scrollbar'
                     style={{ transform: `translate3d(${translateX}px, 0px, 0px)` }}
                 >
                     {items && items.length
