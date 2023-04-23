03677e1edbd0ec16561ce90aab6a7cc571c1323f
Boydem <noyemdahan@gmail.com>
2023-04-23 11:52:40 +0300

    mobile test

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,14 @@
+import { useLayoutEffect, useState } from 'react'
+
+export function useWindowSize() {
+    const [size, setSize] = useState({ vw: 0, vh: 0 })
+    useLayoutEffect(() => {
+        function updateSize() {
+            setSize({ vw: window.innerWidth, vh: window.innerHeight })
+        }
+        window.addEventListener('resize', updateSize)
+        updateSize()
+        return () => window.removeEventListener('resize', updateSize)
+    }, [])
+    return size
+}
