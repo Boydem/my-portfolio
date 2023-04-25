376850cbdfaf23e68e85683fe127be2f3af1f3ba
Boydem <noyemdahan@gmail.com>
2023-04-25 11:41:16 +0300

    improved cursor cmp calculations

--- src/components/Cursor/Cursor.tsx
+++ src/components/Cursor/Cursor.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { BsPlus } from 'react-icons/bs'
 import { RootState } from '../../store/store'
 import { useSelector } from 'react-redux'
@@ -9,33 +9,44 @@ export function Cursor() {
     const [lastHoveredTarget, setLastHoveredTarget] = useState<Element | null>(null)
     const [isHover, setIsHover] = useState<boolean>(false)
     const [isLinkHover, setIsLinkHover] = useState<boolean>(false)
-
-    const handleHoverEffects = (target: Element | null) => {
-        // Check if the target or any of its parents have the data-hover attribute
-        const parentWithDataHover = target ? target.closest('[data-hover="true"]') : null
-        if (lastHoveredTarget !== parentWithDataHover) {
-            setIsHover(!!parentWithDataHover)
-        }
-
-        // Check if the target or any of its parents have the data-link-hover attribute
-        const parentWithDataLinkHover = target ? target.closest('[data-link-hover="true"]') : null
-        if (lastHoveredTarget !== parentWithDataLinkHover) {
-            setIsLinkHover(!!parentWithDataLinkHover)
-        }
-        setLastHoveredTarget(parentWithDataHover || parentWithDataLinkHover || null)
-    }
+    const cursorRef = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => {
+        const handleHoverEffects = (target: Element | null) => {
+            // Check if the target or any of its parents have the data-hover attribute
+            const parentWithDataHover = target ? target.closest('[data-hover="true"]') : null
+            if (lastHoveredTarget !== parentWithDataHover) {
+                setIsHover(!!parentWithDataHover)
+            }
+
+            // Check if the target or any of its parents have the data-link-hover attribute
+            const parentWithDataLinkHover = target ? target.closest('[data-link-hover="true"]') : null
+            if (lastHoveredTarget !== parentWithDataLinkHover) {
+                setIsLinkHover(!!parentWithDataLinkHover)
+            }
+            setLastHoveredTarget(parentWithDataHover || parentWithDataLinkHover || null)
+        }
         handleHoverEffects(target)
     }, [target])
 
     return (
         <motion.div
             className='cursor'
-            animate={{
-                x: window.innerWidth > mousePos.x + 20 ? mousePos.x : window.innerWidth - 20,
-                y: window.innerHeight > mousePos.y + 20 ? mousePos.y : window.innerHeight - 20,
-            }}
+            ref={cursorRef}
+            animate={
+                cursorRef.current
+                    ? {
+                          x:
+                              window.innerWidth > mousePos.x + cursorRef.current.offsetWidth / 2
+                                  ? mousePos.x - cursorRef.current.offsetWidth / 2
+                                  : window.innerWidth - cursorRef.current.offsetWidth / 2,
+                          y:
+                              window.innerHeight > mousePos.y + cursorRef.current.offsetHeight / 2
+                                  ? mousePos.y - cursorRef.current.offsetHeight / 2
+                                  : window.innerHeight - cursorRef.current.offsetHeight / 2,
+                      }
+                    : { x: 0, y: 0 }
+            }
             transition={{ duration: 0.1, ease: cubicBezier(0, 1, 0, 1) }}
         >
             <div className={`cursor-shape ${isHover ? 'hover' : ''} ${isLinkHover ? 'link-hover' : ''}`}></div>
